fdfdaf6001e88b9c4a753ec4b1d5d8af90da8804
bopes <clcblee@gmail.com>
2016-10-30 12:15:23 -0700

    Connect to db

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
   TESTING = False
   CSRF_ENABLED = True
   SECRET_KEY = '9B6C62635328EEB51BE9AEB85C1AF'
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
   DEBUG = False
