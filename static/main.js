cbd4f82e659c097ecb31213ed0652bc75b65d968
bopes <clcblee@gmail.com>
2016-10-30 18:28:36 -0700

    Update UI

--- static/main.js
+++ static/main.js
@@ -4,6 +4,11 @@
   .controller('WordcountController', ['$scope','$log', '$http', '$timeout',
 
     function($scope, $log, $http, $timeout){
+
+      $scope.submitButtonText = 'Submit';
+      $scope.loading = false;
+      $scope.urlerror = false;
+
       $scope.getResults = function(){
        $log.log("test");
        // get URL from input
@@ -12,7 +17,10 @@
        $http.post('/start', {"url": userInput}).
         success(function(results){
           $log.log(results);
-          getWordCount(results)
+          getWordCount(results);
+          $scope.wordcounts = null;
+          $scope.loading = true;
+          $scope.submitButtonText = "Loading...";
         }).
         error(function(error){
           $log.log(error);
@@ -22,17 +30,26 @@
     function getWordCount(jobID){
       var timeout = "";
       var poller = function(){
-        $http.get('/results/'+jobID).
-          success(function(data, status, headers, config){
+        $http.get('/results/'+jobID)
+          .success(function(data, status, headers, config){
             if (status === 202) {
               $log.log(data, status);
             } else if (status === 200) {
               $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
+              $scope.urlerror = false;
               $scope.wordcounts = data;
               $timeout.cancel(timeout);
               return false;
             }
             timeout = $timeout(poller, 2000)
+          })
+          .error(function(error){
+            $log.log(error);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit";
+            $scope.urlerror = true;
           });
       };
       poller();
