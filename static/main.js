b19b51b12c233ac588a1ca32a685fc74feca4b0c
bopes <clcblee@gmail.com>
2016-10-30 17:37:59 -0700

    Add getWordCount

--- static/main.js
+++ static/main.js
@@ -1,21 +1,40 @@
 (function(){
   'use strict';
   angular.module('WordcountApp', [])
-  .controller('WordcountController', ['$scope','$log', '$http',
-    function($scope, $log, $http){
-    $scope.getResults = function(){
-     $log.log("test");
-     // get URL from input
-     var userInput = $scope.url;
-     // fire the API request
-     $http.post('/start', {"url": userInput}).
-      success(function(results){
-        $log.log(results);
-      }).
-      error(function(error){
-        $log.log(error);
-      });
+  .controller('WordcountController', ['$scope','$log', '$http', '$timeout',
+
+    function($scope, $log, $http, $timeout){
+      $scope.getResults = function(){
+       $log.log("test");
+       // get URL from input
+       var userInput = $scope.url;
+       // fire the API request
+       $http.post('/start', {"url": userInput}).
+        success(function(results){
+          $log.log(results);
+          getWordCount(results)
+        }).
+        error(function(error){
+          $log.log(error);
+        });
     };
+    function getWordCount(jobID){
+      var timeout = "";
+      var poller = function(){
+        $http.get('/results/'+jobID).
+          success(function(data, status, headers, config){
+            if (status === 202) {
+              $log.log(data, status);
+            } else if (status === 200) {
+              $log.log(data);
+              $timeout.cancel(timeout);
+              return false;
+            }
+            timeout = $timeout(poller, 2000)
+          });
+      };
+      poller();
+    }
   }
   ]);
 }());
